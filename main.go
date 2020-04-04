67eccfb8bd005f1928de63821b8a62fbb7804e66
Giang Vu <gvu0110@gmail.com>
2020-04-04 14:56:51 -0400

    Added logo

--- main.go
+++ main.go
@@ -35,8 +35,23 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	http.FileServer(http.Dir("./static"))
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		http.Redirect(w, r, "/error.html", 404)
+	}
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/adduser", addUser)
 	http.ListenAndServe(":8000", nil)
 }

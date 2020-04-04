3af3290a000930f466db21ccbb2bc907502ed1ce
Giang Vu <gvu0110@gmail.com>
2020-04-04 02:38:35 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# volambacmy.com
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var user User
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	f, err := os.OpenFile("addUser.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	user.Username = r.FormValue("username")
+	user.Password = r.FormValue("password")
+
+	logger := log.New(f, "", log.LstdFlags)
+	logger.Printf("New user: %#v", user)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/adduser", addUser)
+	http.ListenAndServe(":8000", nil)
+}
